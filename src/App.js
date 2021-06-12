6490d3c85dc06a733cf14d29b1364c505e0e6242
faruqismael <farusmith5@gmail.com>
2021-06-12 19:49:46 +0300

    using router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Teams from "./components/Teams";
+import Landing from "./components/Landing";
+import Services from "./components/Services";
+import Members from "./components/Members";
+import Footer from "./components/Footer";
 import "./App.css";
 import "./components/styles/tailwind.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Teams />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/members">
+            <Members />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
