fb720608f3153817e81f5c6092e585ddd03225d2
faruqismael <farusmith5@gmail.com>
2021-06-12 18:43:32 +0300

    starter components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,56 @@ import "./styles/Header.css";
 const Header = () => {
   return (
     <nav>
-      <div className="header__left">
-        <h1 className="header__logo">HIGHCV</h1>
-        <a>TEMPLATES</a>
-        <a>PRICING</a>
-      </div>
-      <div className="header__right">
-        <h3>jjj</h3>
+      <div class="bg-indigo-900 px-4 py-4">
+        <div class="md:max-w-6xl md:mx-auto md:flex md:items-center md:justify-between">
+          <div class="flex justify-between items-center">
+            <a href="/" class="inline-block py-2 text-white text-xl font-bold">
+              HIGHTECH
+            </a>
+            <div class="inline-block cursor-pointer md:hidden">
+              <div class="bg-gray-400 w-8 mb-2" style={{ height: "2px" }}></div>
+              <div class="bg-gray-400 w-8 mb-2" style={{ height: "2px" }}></div>
+              <div class="bg-gray-400 w-8" style={{ height: "2px" }}></div>
+            </div>
+          </div>
+
+          <div>
+            <div class="hidden md:block">
+              <a
+                href="/"
+                class="inline-block py-1 md:py-4 text-gray-100 mr-6 font-bold"
+              >
+                Services
+              </a>
+              <a
+                href="/"
+                class="inline-block py-1 md:py-4 text-gray-500 hover:text-gray-100 mr-6"
+              >
+                About
+              </a>
+              <a
+                href="/"
+                class="inline-block py-1 md:py-4 text-gray-500 hover:text-gray-100"
+              >
+                Teams
+              </a>
+            </div>
+          </div>
+          <div class="hidden md:block">
+            <a
+              href="/"
+              class="inline-block py-1 md:py-4 text-gray-500 hover:text-gray-100 mr-6"
+            >
+              Blog
+            </a>
+            <a
+              href="/"
+              class="inline-block py-2 px-4 text-gray-700 bg-white hover:bg-gray-100 rounded-lg"
+            >
+              Contact Us
+            </a>
+          </div>
+        </div>
       </div>
     </nav>
   );
---
+++ src/components/Members.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import img from "./img.jpg";
+
+const Members = () => {
+  let arr = [2, 3, 3, 4, 1, 2, 3, 4, 3];
+  return (
+    <div className="bg-gray-900">
+      <h1 className="text-white text-2xl text-center text-bold py-8">
+        MEMBERS
+      </h1>
+      <div
+        className="
+      grid grid-cols-1
+      gap-6
+      p-16
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-4
+      xl:grid-cols-5"
+      >
+        {arr.map((a) => (
+          <div
+            className="
+        grid grid-cols-1
+        gap-6
+        sm:grid-cols-2
+        md:grid-cols-2
+        lg:grid-cols-4
+        xl:grid-cols-4
+      "
+          >
+            <div className="relative bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+              <img
+                src={img}
+                alt="img"
+                width="75"
+                style={{
+                  objectFit: "contain",
+                }}
+                className="
+            text-white
+            flex
+            items-center
+            absolute
+            rounded-full
+            shadow-2xl
+            left-24
+            -top-6
+          "
+              />
+
+              <div className="mt-20">
+                <p className="text-xl font-semibold my-2">Faruq Ismael</p>
+                <div className="flex space-x-2 text-gray-400 text-sm">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  <p>Marketing Team</p>
+                </div>
+                <div className="flex space-x-2 text-gray-400 text-sm my-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                    />
+                  </svg>
+                  <p>1 Weeks Left</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          // </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Members;
