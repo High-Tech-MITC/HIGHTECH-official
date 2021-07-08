747efe557afb65b496e72b654efe59c8cb7dc137
kenean <keneanalemayehu50@gmail.com>
2021-07-08 23:41:31 +0300

    refactor: update footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,108 +2,134 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div class="table bg-black py-20 text-white  w-full p-4 md:pl-24">
-        <div class="block sm:table-cell">
-          <p class="uppercase text-gray text-xl sm:mb-6">Links</p>
-          <ul class="list-reset text-xs mb-6">
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                FAQ
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Help
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Support
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="block sm:table-cell">
-          <p class="uppercase text-grey text-xl sm:mb-6">Legal</p>
-          <ul class="list-reset text-xs mb-6">
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Terms
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Privacy
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="block sm:table-cell">
-          <p class="uppercase text-grey text-xl sm:mb-6">Projects</p>
-          <ul class="list-reset text-xs mb-6">
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                InQuery
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                TEvaluation
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                RATOOL
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="block sm:table-cell">
-          <p class="uppercase text-grey text-xl sm:mb-6">Social</p>
-          <ul class="list-reset text-xs mb-6">
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Facebook
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Linkedin
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Twitter
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="block sm:table-cell">
-          <p class="uppercase text-grey text-xl sm:mb-6">Company</p>
-          <ul class="list-reset text-xs mb-6">
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Official Blog
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                About Us
-              </a>
-            </li>
-            <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-              <a href="/" class="text-grey hover:text-grey-dark">
-                Contact
-              </a>
-            </li>
-          </ul>
+    <footer class="text-gray-600 body-font bg-indigo-900">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap md:text-left text-center order-first">
+          <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">
+              Company
+            </h2>
+            <nav class="list-none mb-10">
+              <li>
+                <a class="text-white hover:text-gray-300">About Us</a>
+              </li>
+              <li>
+                <a class="text-white hover:text-gray-300">Blog</a>
+              </li>
+              <li>
+                <a class="text-white hover:text-gray-300">Contact</a>
+              </li>
+            </nav>
+          </div>
+
+          <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">
+              Projects
+            </h2>
+            <nav class="list-none mb-10">
+              <li>
+                <a class="text-white hover:text-gray-300">InQuery</a>
+              </li>
+              <li>
+                <a class="text-white hover:text-gray-300">TEvaluation</a>
+              </li>
+              <li>
+                <a class="text-white hover:text-gray-300">RATOOL</a>
+              </li>
+            </nav>
+          </div>
+
+          <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h1 class="title-font font-medium text-white tracking-widest text-sm mb-3">
+              SUBSCRIB to out newsletter to get notified about new events and
+              insightful blogs
+            </h1>
+            <div class="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
+              <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                <label for="footer-field" class="leading-7 text-sm text-white">
+                  Email Address
+                </label>
+                <input
+                  type="text"
+                  id="footer-field"
+                  name="footer-field"
+                  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-gray-500 text-base outline-none text-gray-200 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                Subscribe
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <h2 className="text-center text-xl pb-8 font-extrabold">
-        © {new Date().getFullYear()}{" "}
-        <span className="text-indigo-900">HighTech</span>
-      </h2>
+      <div class="bg-gray-100">
+        <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+          <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <span class="ml-3 text-xl">HIGHTECH</span>
+          </a>
+          <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
+            © 2021 Hightech - All Right Reserved.
+          </p>
+          <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <a class="text-gray-500">
+              <svg
+                fill="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a class="ml-3 text-gray-500">
+              <svg
+                fill="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a class="ml-3 text-gray-500">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+            <a class="ml-3 text-gray-500">
+              <svg
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="0"
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="none"
+                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                ></path>
+                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              </svg>
+            </a>
+          </span>
+        </div>
+      </div>
     </footer>
   );
 };
