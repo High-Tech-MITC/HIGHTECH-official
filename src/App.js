7accd3b456f8b43c485102ba387b95683c9fa8ff
kenean <keneanalemayehu50@gmail.com>
2021-07-08 21:57:19 +0300

    refactor: install tailwind with npm

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./App.css";
-import "./components/styles/tailwind.css";
 
 const App = () => {
   return (
